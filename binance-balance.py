18de0a39f1d307307b66105dfe15d956eb4f893b
Kyle Briggs <kbrig035@uottawa.ca>
2018-02-27 15:59:37 -0500

    UI tweaks and balance checking

--- binance-balance.py
+++ binance-balance.py
@@ -4,6 +4,7 @@ import tkFileDialog
 import pandas as pd
 from binance.client import Client
 import numpy as np
+from datetime import datetime
 
 
 class BalanceGUI(tk.Frame):
@@ -18,7 +19,7 @@ class BalanceGUI(tk.Frame):
         self.portfolio_view = tk.LabelFrame(parent, text='Portfolio')
         self.portfolio_view.grid(row=0,column=0, sticky=tk.E+tk.W)
         self.portfolio = ttk.Treeview(self.portfolio_view)
-        self.portfolio['columns']=('Stored','Exchange', 'Target','Actual', 'Action')
+        self.portfolio['columns']=('Stored','Exchange', 'Target','Actual', 'Action', 'Status')
         for label in self.portfolio['columns']:
             if label != 'Action':
                 self.portfolio.column(label, width=100)
@@ -43,7 +44,7 @@ class BalanceGUI(tk.Frame):
         self.login.grid(row=0, column=4, sticky=tk.E+tk.W)
 
         
-        self.refresh_button = tk.Button(self.controls_view, text='Refresh', command=self.refresh, state='disabled')
+        self.refresh_button = tk.Button(self.controls_view, text='Refresh', command=self.populate_portfolio, state='disabled')
         self.refresh_button.grid(row=1,column=2, sticky=tk.E+tk.W)
         self.dryrun_button = tk.Button(self.controls_view, text='Dry Run', command=self.dryrun, state='disabled')
         self.dryrun_button.grid(row=1,column=3, sticky=tk.E+tk.W)
@@ -67,7 +68,7 @@ class BalanceGUI(tk.Frame):
         self.stream_view = tk.LabelFrame(parent, text='Current State')
         self.stream_view.grid(row=2, column=0, sticky=tk.E+tk.W)
         self.commands = tk.StringVar()
-        self.commands.set('Ready')
+        self.commands.set('{0}: Ready'.format(datetime.today().replace(microsecond=0)))
         self.stream = tk.Label(self.stream_view, textvariable = self.commands, justify=tk.LEFT)
         self.stream.grid(row=0, column=0, sticky=tk.E+tk.W)
 
@@ -86,25 +87,27 @@ class BalanceGUI(tk.Frame):
         self.orderopt['state'] = 'normal'
         self.trade_currency_opt['state'] = 'normal'
 
-        self.update_commands('Logging in...')
+        self.update_commands('{0}: Logging in'.format(datetime.today().replace(microsecond=0)))
         self.client = Client(api_key, api_secret)
         status = self.client.get_system_status()
-        self.update_commands('System status: {0}'.format(status['msg']))
+        self.update_commands('{0}: System status: {1}'.format(datetime.today().replace(microsecond=0), status['msg']))
         
         self.populate_portfolio()
 
 
     def update_commands(self, string):
         self.commands.set(self.commands.get() + '\n' + string)
+        with open('binance_balance_log.log','a') as f:
+            f.write('\n' + string)
                           
     def dryrun(self):
         self.rebalance_button['state'] = 'normal'
         
     def currency_change(self, event):
-        self.coins = self.coins_base
         self.populate_portfolio()
         
     def populate_portfolio(self):
+        self.coins = self.coins_base
         self.portfolio.delete(*self.portfolio.get_children())
         exchange_coins = []
         prices = self.client.get_all_tickers()
@@ -128,16 +131,12 @@ class BalanceGUI(tk.Frame):
         self.coins.loc[:,'actual'] *= 100.0/total
 
         print self.coins
-        self.update_commands('Portfolio Value: {0:.6f} {1}'.format(total, self.trade_currency.get()))
-        self.update_commands('Portfolio RMS deviation: {0:.2f}%'.format(np.sqrt(np.average((self.coins.actual.values - self.coins.allocation.values)**2))))     
+        self.update_commands('{0}: Portfolio Value: {1:.6f} {2}'.format(datetime.today().replace(microsecond=0), total, self.trade_currency.get()))
         i = 0
         for row in self.coins.itertuples():
-            self.portfolio.insert("" , i, text=row.coin, values=(row.fixed_balance, row.exchange_balance, '{0} %'.format(row.allocation), '{0:.2f} %'.format(row.actual), 'None'))
+            self.portfolio.insert("" , i, text=row.coin, values=(row.fixed_balance, row.exchange_balance, '{0} %'.format(row.allocation), '{0:.2f} %'.format(row.actual), '', 'Waiting'))
             i += 1
         
-    def refresh(self):
-        pass
-
     def rebalance(self):
         self.rebalance_button['state'] = 'disabled'
     
