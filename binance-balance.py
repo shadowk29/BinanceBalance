296c38f2e9e7ce36396e8d4d7f70d136cca90bc5
Kyle Briggs <kbrig035@uottawa.ca>
2018-02-27 11:14:00 -0500

    added order type selector

--- binance-balance.py
+++ binance-balance.py
@@ -35,8 +35,8 @@ class BalanceGUI(tk.Frame):
         self.key_entry.grid(row=0, column=1,sticky=tk.E+tk.W)
         self.secret_entry = tk.Entry(self.controls_view, show='*')
         self.secret_entry.grid(row=0, column=3,sticky=tk.E+tk.W)
-        enter = tk.Button(self.controls_view, text='Enter', command = self.api_enter)
-        enter.grid(row=0, column=4, sticky=tk.E+tk.W)
+        self.login = tk.Button(self.controls_view, text='Login', command = self.api_enter)
+        self.login.grid(row=0, column=4, sticky=tk.E+tk.W)
 
         
         self.refresh_button = tk.Button(self.controls_view, text='Refresh', command=self.refresh, state='disabled')
@@ -44,6 +44,11 @@ class BalanceGUI(tk.Frame):
         self.rebalance_button = tk.Button(self.controls_view, text='Rebalance', command=self.rebalance, state='disabled')
         self.rebalance_button.grid(row=1,column=2, columnspan=2, sticky=tk.E+tk.W)
 
+        self.ordertype = tk.StringVar()
+        self.ordertype.set('Market')
+        self.orderopt = tk.OptionMenu(self.controls_view, self.ordertype, 'Market', 'Adaptive Limit', 'Median Limit')
+        self.orderopt.grid(row=1, column=4, stick=tk.E+tk.W)
+        self.orderopt['state'] = 'disabled'
         
 
         #streaming display
@@ -52,9 +57,6 @@ class BalanceGUI(tk.Frame):
         self.commands = tk.StringVar()
         self.stream = tk.Label(self.stream_view, textvariable = self.commands, bg='black')
         self.stream.grid(row=0, column=0, sticky=tk.E+tk.W)
-
-        
-
         
 
     def api_enter(self):
@@ -64,12 +66,12 @@ class BalanceGUI(tk.Frame):
         self.secret_entry.delete(0,'end')
 
         
-
-        
         self.key_entry['state'] = 'disabled'
         self.secret_entry['state'] = 'disabled'
+        self.login['state'] = 'disabled'
         self.refresh_button['state'] = 'normal'
         self.rebalance_button['state'] = 'normal'
+        self.orderopt['state'] = 'normal'
         self.populate_portfolio()
         
 
