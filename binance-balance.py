062a4f36c7c42142a5bce1a889635305d6d8576c
Kyle Briggs <kbrig035@uottawa.ca>
2018-06-16 22:27:40 -0400

    more bugdfixes

--- binance-balance.py
+++ binance-balance.py
@@ -271,7 +271,6 @@ class BalanceGUI(tk.Frame):
         self.update_status()
         i = 0
         for row in self.coins.itertuples():
-            print row.coin
             self.portfolio.insert('' ,
                                   i,
                                   iid=row.coin,
@@ -365,15 +364,13 @@ class BalanceGUI(tk.Frame):
             if coin in coins:
                 exchange_balance = float(balance['f']) + float(balance['l'])
                 locked_balance = float(balance['l'])
-                exchange_balance = round_decimal(exchange_balance,self.coins.loc[self.coins['coin'] == coin]['stepsize'].values[0])
-                locked_balance = round_decimal(locked_balance,self.coins.loc[self.coins['coin'] == coin]['stepsize'].values[0])
-                self.portfolio.set(coin, column='Exchange', value=exchange_balance)
-                self.portfolio.set(coin, column='Locked', value=locked_balance)
+                self.portfolio.set(coin, column='Exchange', value=round_decimal(exchange_balance,self.coins.loc[self.coins['coin'] == coin]['stepsize'].values[0])
+                self.portfolio.set(coin, column='Locked', value=round_decimal(locked_balance,self.coins.loc[self.coins['coin'] == coin]['stepsize'].values[0]))
                 self.coins.loc[self.coins['coin'] == coin, 'exchange_balance'] = exchange_balance
                 self.coins.loc[self.coins['coin'] == coin, 'locked_balance'] = locked_balance
                 ask = self.coins.loc[self.coins['coin'] == coin, 'askprice'].values[0]
-                value = (float(self.coins.loc[self.coins['coin'] == coin, 'exchange_balance'].values[0]) +
-                         float(self.coins.loc[self.coins['coin'] == coin, 'fixed_balance'].values[0])) * ask
+                value = (self.coins.loc[self.coins['coin'] == coin, 'exchange_balance'].values[0] +
+                         self.coins.loc[self.coins['coin'] == coin, 'fixed_balance'].values[0]) * ask
                 self.coins.loc[self.coins['coin'] == coin, 'value'] = value
 
         self.total = np.sum(self.coins['value']) 
@@ -398,8 +395,8 @@ class BalanceGUI(tk.Frame):
         self.coins.loc[self.coins['coin'] == coin, 'askprice'] = ask
         self.portfolio.set(coin, column='Bid', value=bidprice)
         self.coins.loc[self.coins['coin'] == coin, 'bidprice'] = bid
-        value = (float(self.coins.loc[self.coins['coin'] == coin, 'exchange_balance'].values[0]) +
-                 float(self.coins.loc[self.coins['coin'] == coin, 'fixed_balance'].values[0])) * ask
+        value = (self.coins.loc[self.coins['coin'] == coin, 'exchange_balance'].values[0] +
+                 self.coins.loc[self.coins['coin'] == coin, 'fixed_balance'].values[0]) * ask
         self.coins.loc[self.coins['coin'] == coin, 'value'] = value
         self.total = np.sum(self.coins['value'])
         self.coins['actual'] = self.coins.apply(lambda row: 100.0 * row.value / self.total, axis=1)
@@ -417,8 +414,8 @@ class BalanceGUI(tk.Frame):
         
         for row in self.coins.itertuples():
             self.process_queue(flush=True)
-            tradecoin_balance = float(np.squeeze(self.coins[self.coins['coin'] == self.trade_coin]['exchange_balance'].values))
-            tradecoin_locked = float(np.squeeze(self.coins[self.coins['coin'] == self.trade_coin]['locked_balance'].values))
+            tradecoin_balance = np.squeeze(self.coins[self.coins['coin'] == self.trade_coin]['exchange_balance'].values)
+            tradecoin_locked = np.squeeze(self.coins[self.coins['coin'] == self.trade_coin]['locked_balance'].values)
             tradecoin_free = tradecoin_balance - tradecoin_locked
             dif = (row.allocation - row.actual) / 100.0 * self.total / row.price
 
