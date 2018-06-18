afbf8787166f0efd307752f687a55603d4ef5c0f
Kyle Briggs <kbrig035@uottawa.ca>
2018-06-18 14:41:42 -0400

    updated messages

--- binance-balance.py
+++ binance-balance.py
@@ -447,7 +447,7 @@ class BalanceGUI(tk.Frame):
             elif side == SIDE_BUY and qty * price > tradecoin_free:
                 status = 'Insufficient ' + self.trade_coin + ' for purchase'
             else:
-                
+                status = 'Trade Ready'
                 trade_type = self.ordertype.get()
                 trade_currency = self.trade_coin
                 try:
